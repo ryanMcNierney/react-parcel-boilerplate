0f98c9d1a4a93fae274aca91a3fcc8fe03ef892a
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-09-07 14:16:00 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './app.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <h1>React App with Parcel</h1>
+      </div>
+    )
+  }
+}
+
+export default App
